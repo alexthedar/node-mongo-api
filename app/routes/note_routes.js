19abd06a388f22ef57cb490b3bdebe71b7ac4a45
alexthedar <alex.castaneda@gmail.com>
2017-03-19 11:30:18 -0700

    post, get and delete

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,39 @@
-module.exports = function(app, db){
-  app.post('/notes', (req, res) => {
-    console.log(req.body)
-    res.send('hello')
+var ObjectID = require('mongodb').ObjectID
+
+module.exports = function(app, db) {
+  app.get('/notes/:id', (req,res) => {
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id)}
+    db.collection('notes').findOne(details, (err, item) => {
+      if(err){
+        res.send({'error': 'an error has occurred'})
+      } else {
+        res.send(item)
+      }
+    })
   })
-}
+
+  app.post('/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id)}
+    db.collection('notes').remove(details, (err, item) => {
+      if(err){
+        res.send({'error': 'an error has occurred'})
+      } else {
+        res.send('Note '+id+' deleted')
+      }
+    })
+  })
+
+};
